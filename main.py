3df1a374f107515cf2a13567678ce589ac59efe2
Duzzenn <109883788+davidanukam@users.noreply.github.com>
2025-12-03 22:18:03 -0500

    Created Database and Database Model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 from pydantic import BaseModel
@@ -13,8 +13,38 @@ from typing import Optional, List
 # Create - DELETE
 
 app = FastAPI()
+app.title="BrainrotAPI"
 
-class Item(BaseModel):
-    id: int
-    name: str
-    price: float
+# Database setup
+engine = create_engine("sqlite:///brainrots.db", connect_args={"check_same_thread":False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Database Model
+class Brainrot(Base):
+    __tablename__="Brainrots"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(100), nullable=False, unique=True)
+    value = Column(String(100), nullable=False)
+
+# Link Database Model to the Engine
+Base.metadata.create_all(engine)
+
+# Pydantic Models (Dataclass)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+get_db()
+
+# Endpoints
+@app.get("/")
+def root():
+    return {"message" : "learning"}
